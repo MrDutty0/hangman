b12d71a8dd5dfcd982498a2f05e6773d54feb85a
Tomas Civilis <96726777+MrDutty0@users.noreply.github.com>
2024-03-08 08:00:28 +0200

    Add method to retrieve letter from user, word file

---
+++ lib/game.rb
@@ -0,0 +1,70 @@
+# frozen_string_literal: true
+
+TOTAL_AVAILABLE_GUESSES = 7
+
+# hangman's game class
+class Game
+  attr_accessor :tried_letters, :guessed_letters, :guessing_word
+
+  def initialize
+    @tried_letters = []
+    @guessed_letters = []
+    @guessing_word = nil
+    @left_guesses = TOTAL_AVAILABLE_GUESSES
+  end
+
+  def choose_guessing_word
+    File.open('words.txt', 'r') do |file|
+      words = file.read.split
+
+      available_words = words.select { |word| word.length >= 5 && word.length <= 12 }
+
+      @guessing_word = available_words.sample
+    end
+  end
+
+  def play
+    choose_guessing_word
+
+    loop do
+      input = guess_letter
+    end
+  end
+
+  def guess_letter
+    error_count = 0
+    prompt_text = 'Please enter a single letter to guess or "exit" to exit the game'
+    error_msg = nil
+
+    loop do
+      puts prompt_text
+      puts "#{error_msg}(#{error_count})" unless error_count.zero?
+
+      input = gets.chomp.downcase
+
+      error_msg = check_input(input)
+
+      if error_msg.nil?
+        return input
+      end
+
+      error_count += 1
+    end
+  end
+
+  private
+
+  def check_input(input)
+    error_msg = nil
+
+    if input != 'exit' && input.length != 1
+      error_msg = 'Please enter a single character or "exit" to exit the game'
+    elsif input =~ /^[^a-z]$/i
+      error_msg = 'Please enter a letter character'
+    elsif tried_letters.include?(input)
+      error_msg = 'You have already tried this letter'
+    end
+
+    error_msg
+  end
+end
